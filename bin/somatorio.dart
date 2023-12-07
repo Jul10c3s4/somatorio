a7c2f53294dcea46e4225515e19e06f18320675d
julio cesar <jcso2@aluno.ifal.edu.br>
2023-12-07 09:23:12 -0300

    image added

--- bin/somatorio.dart
+++ bin/somatorio.dart
@@ -1,12 +1,19 @@
 import 'package:somatorio/calcular_somatorio.dart' as somatorio;
 import 'package:somatorio/input.dart';
 import 'package:somatorio/calcular_somatorio.dart';
+import 'dart:io';
 
 void main(List<String> arguments) {
-  int numero = input("Informe um número: ");
+  String resp;
+  do {
+    int numero = input("Informe um número: ");
 
-  int somatorio = calcularSomatorio(numero);
+    int somatorio = calcularSomatorio(numero);
 
-  print(
-      'O somatório de todos os valore inteiros divisíveis por 3 ou 5 que são inferiores a $numero é: $somatorio');
+    print(
+        'O somatório de todos os valore inteiros divisíveis por 3 ou 5 que são inferiores a $numero é: $somatorio');
+    print("\ndeseja inserir mais algum número ? s para sim, n para não: ");
+    resp = stdin.readLineSync() ?? '';
+  } while (resp == 's');
+  print('Código finalizado...');
 }
